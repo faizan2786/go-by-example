a9d9ca679ca20c54be9496bf5b4ee73f96161180
Faizan Patel <faizan2786@hotmail.com>
2026-01-11 21:08:55 +0000

    fix test for send using fake round tripper

--- hit/hit_test.go
+++ hit/hit_test.go
@@ -1,29 +1,66 @@
 package hit
 
 import (
+	"context"
 	"net/http"
 	"testing"
 	"time"
 )
 
+// define a test http request
+func getTestHttpRequest() *http.Request {
+	// define a test http request
+	req, err := http.NewRequest(http.MethodGet, "/", http.NoBody)
+	if err != nil {
+		panic("Failed to get a new http GET request")
+	}
+	return req
+}
+
+// test Send function (using a fake round tripper)
+
+// define a function type that satisfies the http RoundTripper interface
+type roundTripperFunc func(*http.Request) (*http.Response, error)
+
+// implement the Roundtrip method of the interface
+func (f roundTripperFunc) RoundTrip(r *http.Request) (*http.Response, error) {
+	return f(r)
+}
+
 func TestSend(t *testing.T) {
 
-	got := Send(nil, nil)
-
-	want := Result{
-		Status:   http.StatusOK,
-		Bytes:    100,
-		Duration: 100 * time.Millisecond,
+	// define a fake round tripper function
+	fakeRoundTripper := func(_ *http.Request) (*http.Response, error) {
+		// return a test status code
+		return &http.Response{
+			StatusCode: http.StatusAccepted,
+		}, nil
 	}
 
-	if got != want {
-		t.Fatalf("Send() = %+v; want = %+v\n", got, want)
+	// create a client with a roundTripperFunc type
+	// (convert fakeRoundTripper to the roundTripperFunc type so that
+	// client can use the fake round tripper as its Transport)
+	client := &http.Client{
+		Transport: roundTripperFunc(fakeRoundTripper), // converts the fakeRoundTripper function to the roundTripperFunc type
+	}
+
+	req := getTestHttpRequest()
+
+	res := Send(client, req)
+
+	if res.Status != http.StatusAccepted {
+		t.Fatalf("want: %d, got: %d\n", http.StatusAccepted, res.Status)
 	}
 }
 
-// Test function for Send()
-func getSendFunc() SendFunc {
-	return func(_ *http.Request) Result {
+// test SendN (using a fake send function)
+func TestSendN(t *testing.T) {
+	const N int = 50
+
+	opts := Options{}
+
+	// define a fake Send() function
+	opts.Send = func(_ *http.Request) Result {
 		const roundTripTime = 10 * time.Millisecond
 		time.Sleep(roundTripTime)
 
@@ -33,24 +70,11 @@ func getSendFunc() SendFunc {
 			Duration: roundTripTime,
 		}
 	}
-}
 
-func getHttpRequest() *http.Request {
-	req, err := http.NewRequest(http.MethodGet, "www.myurl.com", http.NoBody)
-	if err != nil {
-		panic("Failed to get a new http GET request for testing")
-	}
-	return req
-}
+	// get the test http request
+	req := getTestHttpRequest()
 
-func TestSendN(t *testing.T) {
-
-	N := 50
-	opts := Options{}
-	opts.Send = getSendFunc()
-	req := getHttpRequest()
-
-	res, err := SendN(N, opts, req)
+	res, err := SendN(context.Background(), N, opts, req)
 	if err != nil {
 		t.Fatalf("SendN() = %v; want no error\n", err)
 	}
