4105c9bbfecea3dfd8dab6339bad83ca023f83f1
Faizan Patel <faizan2786@hotmail.com>
2026-01-12 19:26:18 +0000

    add request duration test using synctest

--- hit/hit_test.go
+++ hit/hit_test.go
@@ -2,8 +2,11 @@ package hit
 
 import (
 	"context"
+	"io"
 	"net/http"
+	"strings"
 	"testing"
+	"testing/synctest"
 	"time"
 )
 
@@ -34,6 +37,10 @@ func TestSend(t *testing.T) {
 		// return a test status code
 		return &http.Response{
 			StatusCode: http.StatusAccepted,
+			Body:       io.NopCloser(strings.NewReader("fake response.")),
+
+			// NoOpCloser wraps the argument and makes it a "Closer" by implementing Close() method with no operation
+			// This is necessary because Body expects a ReadCloser and strings.NewReader returns just a Reader
 		}, nil
 	}
 
@@ -49,8 +56,42 @@ func TestSend(t *testing.T) {
 	res := Send(client, req)
 
 	if res.Status != http.StatusAccepted {
-		t.Fatalf("want: %d, got: %d\n", http.StatusAccepted, res.Status)
+		t.Errorf("got: status = %d, want: %d\n", res.Status, http.StatusAccepted)
 	}
+	if res.Bytes != int64(len("fake response.")) {
+		t.Errorf("got: %d bytes, want: %d\n", res.Bytes, len("fake response."))
+	}
+}
+
+// test send function with time duration in the response.
+// we use time.synctest package which runs tests in a "bubble".
+// Specifically, it mocks time by allowing time to advance by an exact required amount
+// (instead of waiting for said time, synctest allows time calls inside the test to jump by said amount)
+// hence, the test is repeatable and fast
+func TestSendWithDuration(t *testing.T) {
+
+	// a round tripper that uses time.Sleep to simulate a long running request
+	fakeRoundTripper := func(_ *http.Request) (*http.Response, error) {
+		time.Sleep(30 * time.Second)
+
+		res := &http.Response{
+			StatusCode: http.StatusInternalServerError,
+		}
+		return res, nil
+	}
+	client := &http.Client{
+		Transport: roundTripperFunc(fakeRoundTripper),
+	}
+
+	// run the test in a "bubble" using synctest
+	synctest.Test(t, func(t *testing.T) {
+
+		res := Send(client, getTestHttpRequest()) // call finishes "instantly" - NOT after 30s
+
+		if res.Duration != (30 * time.Second) {
+			t.Errorf("got: %v duration, want: %v\n", res.Duration, 30*time.Second)
+		}
+	})
 }
 
 // test SendN (using a fake send function)
@@ -86,6 +127,6 @@ func TestSendN(t *testing.T) {
 	}
 
 	if gotN != N {
-		t.Fatalf("SendN() returned %d results; want %d\n results", gotN, N)
+		t.Errorf("SendN() returned %d results; want %d\n results", gotN, N)
 	}
 }
